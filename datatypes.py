097fc7e0944fd44e6ce9efac9390a1d280758017
rubyamanda-claire <rubyamanda.mwago@gmail.com>
2026-02-10 16:31:36 +0300

    new commit

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+
+age = 18 #Integer
+temperature = 34.89 #Float
+greeting = "Hey There" #String
+isMarried = True #Boolean
+
+print("I am",age,"years old")
+print("The current body temperature of the patient is",temperature,"degrees celsius")
+print(greeting,"Ruby")
+print("Are you married?",isMarried)
+
+#Data Structures - Multiple values stored in a single variable
+cars = ["Audi","BMW","Toyota","Mercedes"] #List - Ordered and Changeable
+fruits = ("Banana", "Mango", "Grapes", "Pineapple") #Tuple - Ordered and Unchangeable
+countries = {"Italy", "Germany","France", "Spain"} #Set - Unordered and Unchangeable
+student = {
+    "firstname" : "Ruby Mwago",
+    "age" : 18,
+    "course" : "FullStack",
+    "gender" : "Male",
+}
+
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["firstname"])
--- variables.py
+++ variables.py
@@ -1,2 +1,15 @@
 #a container used to store a value
 #a name that points out to a value
+firstname = "Ruby"
+school = "emobilis"
+lastname = "Mwago"
+num1 = 56
+num2 = 20
+language = "Python"
+
+print(firstname)
+print(school)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(num1 + num2)
+print(language,"is a web programming language")
