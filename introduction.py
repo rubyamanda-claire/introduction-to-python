f8300ed7ddb9b5af457a69d8df3ebe3606d324e4
rubyamanda-claire <rubyamanda.mwago@gmail.com>
2026-02-10 10:47:34 +0300

    first python

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display an output.
+
+print("Welcome to Python Programming!!")
+print("Ruby")
+#print(100)
+print(30+20)
---
+++ variables.py
@@ -0,0 +1,2 @@
+#a container used to store a value
+#a name that points out to a value
